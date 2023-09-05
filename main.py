6cce32f47dfe95e16572a08f3ac22d99b2a4e3dd
nein4matic <nein4matic@gmail.com>
2023-09-05 12:38:48 +0600

    Main file to start programm, to Page add only one block

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
-""" Flet Search Bar """
-# modules
-import flet
-from flet import *
-import asyncio
-import aiosqlite
-import search
+from testingSpace import MainBlock
+from flet import (app,
+                  Page,
+                  ThemeMode
+                  )
+
 
 def main(page: Page):
+
     page.horizontal_alignment = "center"
     page.vertical_alignment = "start"
-    page.padding = padding.only(top=200)
     page.theme_mode = ThemeMode.LIGHT
+    page.window_min_width = 390
+    page.window_min_height = 600
 
     page.add(
-        search.DropDownSearchBar(),
+        MainBlock()
+        # TopNavigationBlock(),
+        # DropDownSearchBar(),
+        # Table(),
     )
     page.update()
 
 
 if __name__ == "__main__":
-    flet.app(target=main)
+    app(target=main)
