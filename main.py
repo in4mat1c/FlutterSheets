24878e6eb86e530eaad78e154ee9b75d725a1330
nein4matic <nein4matic@gmail.com>
2023-09-07 12:42:31 +0600

    Add route system and realise Register page and Table page

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
-from testingSpace import MainBlock
+from flet import *
+from app_structure import App
 from flet import (app,
+                  Column,
                   Page,
-                  ThemeMode
+                  ThemeMode,
+                  View
                   )
+from register_app import Register
+from user import User
 
 
 def main(page: Page):
 
-    page.horizontal_alignment = "center"
-    page.vertical_alignment = "start"
+    # Check client storage keys. If keys exist, add user and route to table
+
     page.theme_mode = ThemeMode.LIGHT
+    current_user = User()
+
+    def route_change(route):
+        page.views.clear()
+        page.views.append(
+            View(
+                "/",
+                vertical_alignment=MainAxisAlignment.CENTER,
+                horizontal_alignment=CrossAxisAlignment.CENTER,
+                controls=[Register(current_user, page)]
+            )
+        )
+        if page.route == "/table":
+            page.views.append(
+                View(
+                    "/table",
+                    vertical_alignment=MainAxisAlignment.CENTER,
+                    horizontal_alignment=CrossAxisAlignment.CENTER,
+                    controls=[App()]
+                    )
+                )
+        page.update()
+
     page.window_min_width = 390
     page.window_min_height = 600
-
-    page.add(
-        MainBlock()
-        # TopNavigationBlock(),
-        # DropDownSearchBar(),
-        # Table(),
-    )
-    page.update()
+    page.bgcolor = colors.WHITE54
+    page.on_route_change = route_change
+    page.go(page.route)
 
 
 if __name__ == "__main__":
