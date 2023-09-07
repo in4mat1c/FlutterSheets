4ee7d954452a8962c022303de5b84712752903fd
nein4matic <nein4matic@gmail.com>
2023-09-07 12:46:57 +0600

    Basic class to work with users

--- user.py
+++ user.py
@@ -1,4 +1,4 @@
 class User:
-    def __init__(self, username, permission):
+    def __init__(self, username='Guest', permission='None'):
         self.username = username
         self.permission = permission
