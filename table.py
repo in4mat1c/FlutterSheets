3ecf5403e456f251f8a65ea75076674f8c507df0
nein4matic <nein4matic@gmail.com>
2023-09-07 12:45:44 +0600

    Change in get_table_values_list function disabled option to read_only

--- table.py
+++ table.py
@@ -1,12 +1,15 @@
 from flet import *
+
+import search
 from database import Database
 from tableDesign import TableDesign
 
+
 class Table(UserControl):
     controls_list = {}
+    database = Database('database')
 
     def __init__(self):
-        self.database = Database('database')
         super().__init__()
 
     @classmethod
@@ -18,10 +21,10 @@ class Table(UserControl):
             controls=[
                 TableDesign.get_table_cell(value[0], disabled),
                 TableDesign.get_table_cell(value[1], disabled),
-                TableDesign.get_table_cell(res_x, True),
+                TableDesign.get_table_cell(res_x, True, colors.GREY_300),
                 TableDesign.get_table_cell(value[2], disabled),
                 TableDesign.get_table_cell(value[3], disabled),
-                TableDesign.get_table_cell(res_y, True),
+                TableDesign.get_table_cell(res_y, True, colors.GREY_300),
             ]
         )
 
@@ -47,18 +50,19 @@ class Table(UserControl):
             except:
                 return 0
 
-    def get_table_values(self, table_name):
-        data = self.database.get_data(table_name)
-        obj = self.controls_list['table']
-        price = self.database.get_price(table_name)
+    @classmethod
+    def get_table_values(cls, table_name):
+        data = cls.database.get_data(table_name)
+        obj = cls.controls_list['table']
+        price = cls.database.get_price(table_name)
         obj.content = TableDesign.top_table(table_name.replace('_', ' '), price)
-        amount = 0
+        amount_x = 0
+        amount_y = 0
         for value in data:
-            x = self.__validate_int(value[0])
-            x1 = self.__validate_int(value[1])
-            y = self.__validate_int(value[2])
-            y1 = self.__validate_int(value[3])
-            print(value)
+            x = cls.__validate_int(value[0])
+            x1 = cls.__validate_int(value[1])
+            y = cls.__validate_int(value[2])
+            y1 = cls.__validate_int(value[3])
 
             if isinstance(x, float) or isinstance(x1, float):
                 res_x = round(x * x1, 2)
@@ -67,13 +71,37 @@ class Table(UserControl):
 
             res_y = round((y * y1) / 1000000, 2)
 
-            amount += round((res_x + res_y), 2)
+            amount_x += res_x
+            amount_y += res_y
 
             # Create main table Design
-            obj.content.controls[3].controls.append(self.__made_table_string(value, False, res_x, res_y))
-            obj.content.controls.append(TableDesign.bottom_table_part(res_x, res_y, amount))
+            obj.content.controls[3].controls.append(cls.__made_table_string(value, False, res_x, res_y))
 
+        obj.content.controls.append(TableDesign.bottom_table_part(round(amount_x, 2), round(amount_y, 2), round(amount_x + amount_y, 2)))
         obj.update()
+
+    @classmethod
+    def get_table_values_list(cls):
+        obj = cls.controls_list['table']
+        main_list = []
+        for rows in obj.content.controls[3].controls:
+            first_list = []
+            for subject in rows.controls:
+                if subject.read_only != True:
+                    first_list.append(subject.value)
+            main_list.append(first_list + [' '])
+        return main_list
+
+    @classmethod
+    def change_cell_value(cls, e):
+        values = cls.get_table_values_list()
+        obj = cls.controls_list['table']
+        current_table = search.DropDownSearchBar.controls_list['table_name']
+        cls.database.delete_data(current_table)
+        for value in values:
+            cls.database.insert_data(current_table, value)
+        cls.get_table_values(current_table)
+        # Update Error Can't update file
 
     # Top Bar Functionality
 
